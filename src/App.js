132576970a907608ffab054d1f5aa28327711572
Your Name <you@example.com>
2023-02-15 21:06:45 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
-  const counter = useSelector((state) => state.counter);
+  const countere = useSelector((state) => state.counters);
+  const dispatch = useDispatch();
+  const increment = () => {dispatch({type: 'INC'});};
+  const decrement = () => {dispatch({type: "DEC"});};
+  const addby = () => {dispatch({type: "ADD", payload: 10});};
   return (
     <div className="App">
       <h1>Counter App</h1>
-      <h2>{counter}</h2>
+      <h2>{countere}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addby}>Add By 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import {createStore} from 'redux'
 
-const reducerDerFn = (state = { counter:0 }, action) => {
+const reducerDerFn = (state = { counters:0 }, action) => {
+   
+   if(action.type === "INC"){
+      return {counters: state.counters+1};
+   }
 
+   if(action.type === "DEC"){
+    return {counters: state.counters-1};
+   }
+
+   if(action.type === "ADD"){
+     return{counters: state.counters + action.payload};
+   }
+   return state;
 };
 
 const store = createStore(reducerDerFn);
 
-export default store;
+export default store; 
